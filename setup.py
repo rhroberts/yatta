8758b53c4860a67184d588d4e9496fa46f1f9926
Rusty Roberts <rust.roberts@protonmai.com>
2020-05-08 10:42:30 -0500

    fix setup error, update dist packes

--- setup.py
+++ setup.py
@@ -17,7 +17,13 @@ setuptools.setup(
     install_requires=['click', 'pyfiglet'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
-        'Development Status :: 1 - Planning'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 1 - Planning',
     ],
+    py_modules=['yatta'],
+    entry_points={
+        'console_scripts': [
+            "yatta=yatta:main"
+        ]
+    }
 )

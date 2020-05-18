fb08e1c12939ab29de9c34fd6d589d5d63ce002b
Rusty Roberts <rust.roberts@protonmail.com>
2020-05-18 00:02:51 -0500

    mostly finished integrated sqlalchemy

--- yatta/db.py
+++ yatta/db.py
@@ -1,142 +1,106 @@
-import sqlite3
+from sqlalchemy import (create_engine, Column, Integer, String, ForeignKey,
+                        DateTime, Interval)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker, relationship
+from appdirs import user_data_dir
+from tabulate import tabulate
 import pandas as pd
+import os
+
+APP_NAME = 'yatta'
+DATA_DIR = user_data_dir(APP_NAME)
+DB_PATH = os.path.join(DATA_DIR, 'yatta.db')
+
+engine = create_engine(f'sqlite:///{DB_PATH}', echo=False)
+Sessionmkr = sessionmaker(bind=engine)
+session = Sessionmkr()
+Base = declarative_base()
 
 
-class DB(object):
-    '''
-    Object representing sqlite database.
-    '''
-    def __init__(self, db_path):
-        self.path = db_path
-        self.connection = sqlite3.connect(self.path)
-        self.cursor = self.connection.cursor()
-        self.result = None
-        self._close()
+# define tables / db objects
+class Task(Base):
+    __tablename__ = 'tasks'
+    # __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    tags = Column(String)
+    description = Column(String)
+    records = relationship('Record', back_populates='task')
+    total = Column(Integer)
 
-    def _open(self):
-        self.connection = sqlite3.connect(self.path)
-        self.cursor = self.connection.cursor()
+    def __repr__(self):
+        return(f'<Task(name={self.name}, tags={self.tags}, ' +
+               f'description={self.description}, total={self.total})>')
 
-    def _close(self):
-        self.connection.close()
-
-    def _commit(self):
-        self.connection.commit()
-
-    def _fetch(self):
-        return(self.cursor.fetchall())
-
-    def execute(self, query, *args):
-        self._open()
-        self.cursor.execute(query, *args)
-        self.result = self._fetch()
-        self._commit()
-        self._close()
+    def __str__(self):
+        s = [['ID', 'Task', 'Tags', 'Description', 'Total'],
+             [self.id, self.name, self.tags, self.description, self.total]]
+        return(tabulate(s, tablefmt='fancy_grid'))
 
 
-class AppDB(DB):
-    '''
-    Subclass of DB specific to this application.
-    '''
-    def __init__(self, db_path):
-        super().__init__(db_path)
-        self._init_database()
+class Record(Base):
+    __tablename__ = 'records'
+    id = Column(Integer, primary_key=True)
+    task_name = Column(String, nullable=False, unique=False)
+    task_id = Column(Integer, ForeignKey('tasks.id'))
+    start = Column(DateTime, nullable=False)
+    end = Column(DateTime, nullable=False)
+    duration = Column(Interval, nullable=False)
 
-    def _init_database(self):
-        create_tasks_table = '''
-            CREATE TABLE if not exists tasks (
-                id INTEGER PRIMARY KEY,
-                task TEXT NOT NULL,
-                tags TEXT NOT NULL DEFAULT '',
-                description TEXT NOT NULL DEFAULT '',
-                start DATETIME NOT NULL,
-                stop DATETIME NOT NULL,
-                duration REAL NOT NULL
-            )
-        '''
-        self.execute(create_tasks_table)
-        create_tasklist_table = '''
-            CREATE TABLE if not exists task_list (
-                id INTEGER PRIMARY KEY,
-                task TEXT NOT NULL,
-                tags TEXT NOT NULL DEFAULT '',
-                description TEXT NOT NULL DEFAULT '',
-                total INTEGER NOT NULL
-            )
-        '''
-        self.execute(create_tasklist_table)
+    task = relationship('Task', back_populates='records')
 
-    def record_task(self, task):
-        query = '''
-            INSERT INTO tasks
-            (task, tags, description, start, stop, duration)
-            VALUES (?, ?, ?, ?, ?, ?)
-        '''
-        self.execute(
-            query,
-            (
-                task.name, task.tags, task.description,
-                task.start, task.end, task.duration
-            )
-        )
+    def __repr__(self):
+        return(f'<Record(task_name={self.task_name}, start={self.start} ' +
+               f'end={self.start}, duration={self.duration.seconds})>')
 
-    def update_task_list(self, task):
-        if not self.check_existing(task):
-            query = '''
-                INSERT INTO task_list
-                (task, tags, description, total)
-                VALUES (?, ?, ?, ?)
-            '''
-            self.execute(
-                query, (task.name, task.tags, task.description, task.duration)
-            )
-        else:
-            query = '''UPDATE task_list SET total = ? WHERE task = ?'''
-            self.execute(query, (self.get_total_time(task), task.name))
-
-    def check_existing(self, task):
-        # check if a task exists in task_list table
-        task_list = self.get_task_list()
-        if task_list.empty:
-            return(False)
-        return(task.name in task_list['task'].to_numpy())
-
-    def get_task_list(self):
-        # get the contents of a task_list table and convert to DataFrame
-        query = '''SELECT * FROM task_list'''
-        self.execute(query)
-        task_list = to_dataframe(self.result)
-        query = '''PRAGMA table_info(task_list)'''
-        self.execute(query)
-        if not task_list.empty:
-            task_list.columns = [res[1] for res in self.result[1:]]
-        return(task_list)
-
-    def get_task_info(self, task):
-        query = '''SELECT * FROM task_list WHERE task = ?'''
-        self.execute(query, (task.name,))
-        id, name, tags, description, total = self.result[0]
-        return(id, name, tags, description, total)
-
-    # FIXME: only updates after task is tracked a second time
-    # FIXME: this all got way too convoluted...
-    def get_total_time(self, task):
-        query = '''SELECT * FROM tasks WHERE task = ?'''
-        self.execute(query, (task.name,))
-        task_history = to_dataframe(self.result)
-        if task_history.empty:
-            return(0)
-        # TODO: store table cols somewhere?
-        query = '''PRAGMA table_info(tasks)'''
-        self.execute(query)
-        task_history.columns = [res[1] for res in self.result[1:]]
-        return(task_history['duration'].sum())
+    def __str__(self):
+        s = [
+                ['Record ID', 'Task ID', 'Task', 'Start', 'End', 'Duration'],
+                [
+                    self.id, self.task_id, self.task_name,
+                    self.start, self.end, self.duration.seconds
+                ]
+            ]
+        return(tabulate(s, tablefmt='fancy_grid'))
 
 
-def to_dataframe(result):
-    if not result:
-        return(pd.DataFrame())
-    # convert query results to dataframe
-    df = pd.DataFrame.from_records(result)
-    df = df.set_index(0, drop=True)
+# add the above mappings to database, if they don't already exist
+Base.metadata.create_all(engine)
+
+
+def add_record(task, record):
+    try:
+        session.add(task)
+        session.commit()
+        print(f'Created new task: [{task.name}]')
+    except IntegrityError:
+        print(f'Task [{task.name}] exists; It\'s record will be updated.')
+        session.rollback()
+
+    task.records.append(record)
+    session.add(record)
+    session.commit()
+
+
+def get_records(taskname=None):
+    if taskname:
+        query = session.query(Record).filter(Record.task_name == taskname)
+    else:
+        query = session.query(Record)
+    return(query)
+
+
+def get_tasks(taskname=None):
+    if taskname:
+        query = session.query(Task).filter(Task.name == taskname)
+    else:
+        query = session.query(Task)
+    return(query)
+
+
+def query_to_df(query):
+    df = pd.read_sql(
+        query.statement, query.session.bind, index_col='id'
+    )
     return(df)

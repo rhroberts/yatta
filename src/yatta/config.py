081adb6cf7715a2ad81527f2944efc5d7791242d
Rusty Roberts <rust.roberts@protonmail.com>
2020-06-26 10:26:13 -0500

    WIP: create config class

---
+++ src/yatta/config.py
@@ -0,0 +1,15 @@
+"""
+A class to handle app configuration.
+"""
+
+
+class Config(object):
+
+    default_config = {
+        "figlet_font": "doom",
+        "table_style": "fancy_grid",
+        "run_in_background": "false"
+    }
+
+    def __init__(self):
+        self.defaults = self.default_config
